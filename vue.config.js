b2613f4b2e16a176ecb0e41e6d5e7556bbd863b4
lizijie <leo.lizijie@outlook.com>
2019-01-16 19:13:55 +0800

    fix disableHostCheck position

--- vue.config.js
+++ vue.config.js
@@ -13,8 +13,8 @@ module.exports = {
             '/ms': {
                 target: 'https://www.easy-mock.com/mock/592501a391470c0ac1fab128',
                 changeOrigin: true
-            },
-            disableHostCheck: true
-        }
+            }
+        },
+        disableHostCheck: true
     }
 };
