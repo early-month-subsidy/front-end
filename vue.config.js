89374fdf1bdbe0ff19e7e940a81d44cf34ff5284
lizijie <leo.lizijie@outlook.com>
2019-01-16 19:09:21 +0800

    fix invalid request header

--- vue.config.js
+++ vue.config.js
@@ -3,17 +3,18 @@ module.exports = {
     productionSourceMap: false,
     devServer: {
         proxy: {
-            '/api':{
-                target:'http://172.18.215.188:5000',
-                changeOrigin:true,
-                pathRewrite:{
-                    '/api':''
+            '/api': {
+                target: 'http://172.18.215.188:5000',
+                changeOrigin: true,
+                pathRewrite: {
+                    '/api': ''
                 }
             },
-            '/ms':{
+            '/ms': {
                 target: 'https://www.easy-mock.com/mock/592501a391470c0ac1fab128',
                 changeOrigin: true
-            }
+            },
+            disableHostCheck: true
         }
     }
 };
